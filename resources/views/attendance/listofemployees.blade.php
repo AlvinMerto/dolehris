c1eb8f279589cd337b3c5a3cfe7bcafa90c4e09b
Alvin Merto <ajbmerto@gmail.com>
2023-06-22 09:03:48 +0800

    22062023 update

--- resources/views/attendance/listofemployees.blade.php
+++ resources/views/attendance/listofemployees.blade.php
@@ -10,6 +10,9 @@
             echo "</td>";
             echo "<td>";
                 echo "<label class='tbl_lbl' for='check_{$p->biometricid}'>".$name."</label>";
+                echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+                // http://127.0.0.1/personnel/administration/940
+                echo "<a href='".route('personneladministration')."/{$p->perid}' target='_blank'> <i class='fa fa-pencil' aria-hidden='true'></i> </a>";
             echo "</td>";
             echo "<td>";
                 echo "<span id='status_{$p->biometricid}'>  </span> ";
